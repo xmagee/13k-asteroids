e9af2746194602afdabd543dfcdcf62186bd02ef
Alex Magee <86931568+xmagee@users.noreply.github.com>
2021-08-17 18:35:39 -0400

    split html and js, updated build script

--- build.js
+++ build.js
@@ -1,28 +1,42 @@
 const minify = require('minify'), 
     fs = require('fs'), 
     path = require('path'),
-    input_file = path.join(path.dirname('../'), 'src/index.html')
-    output_dir = 'build/', 
-    output_file = `${output_dir}index.html`
+    output_dir = 'build/';
 
-minify(input_file, {
+if (!fs.existsSync(output_dir)) {
+    fs.mkdirSync(output_dir);
+} else {
+    fs.rmdirSync(output_dir, { recursive: true });
+    fs.mkdirSync(output_dir);
+}
+
+minify(path.join(path.dirname('../'), 'src/index.html'), {
     html: {
         removeAttributeQuotes: false,
-        removeOptionalTags: false,  
-        ecma: 5,
+        removeOptionalTags: false,
     }})
-    .then(minified => {
-        if (!fs.existsSync(output_dir)) {
-            fs.mkdirSync(output_dir)
-        } else if (fs.existsSync(output_file)) {
-            fs.rmSync(output_file)
-        }
-
+    .then(m => {
+        var output_file = `${output_dir}/index.html`;
         try {
-            fs.writeFileSync(output_file, minified)
-            console.log(`build complete, final size: ${fs.statSync(output_file).size / 1000.0} KB`)    
+            fs.writeFileSync(output_file, m);
+            console.log(`minified html: ${fs.statSync(output_file).size / 1000.0} KB`)   ; 
         } catch (err) { 
-            console.log(err)
+            console.log(err);
         }
     })
-    .catch(console.error)
+    .catch(console.error);
+
+minify(path.join(path.dirname('../'), 'src/index.js'), {
+    js: {
+        ecma: 5,
+    }})
+    .then(m => {
+        var output_file = `${output_dir}/index.js`;
+        try {
+            fs.writeFileSync(output_file, m);
+            console.log(`minified js: ${fs.statSync(output_file).size / 1000.0} KB`);
+        } catch (err) { 
+            console.log(err);
+        }
+    })
+    .catch(console.error);
